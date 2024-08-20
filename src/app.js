03a4fabe63ff99e55bfec063d688ee6f369300e5
wanderley <camposwanderley418@gmail.com>
2024-08-20 10:18:36 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import routes from './routes'
 import './database'
+import cors from 'cors'
 
 class App {
   constructor() {
     this.app = express()
+    this.app.use(cors())
 
     this.middlewares()
     this.routes()
